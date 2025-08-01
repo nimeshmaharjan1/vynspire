b44dc3a95c21fde412213faf208774bff0bb5267
Nimesh Maharjan <maharjannimesh11@gmail.com>
2025-08-01 18:04:17 +0000

    feat: initialize prisma and setup the schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<div className="p-26">
+			<p className="text-4xl font-bold">Home Page</p>
+			<Button>Click Me</Button>
+		</div>
+	);
 };
 
 export default HomePage;
